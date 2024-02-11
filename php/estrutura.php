40f0ad022cd7d10f7d2e51d2c27850fa83859036
Igor <igor_stm@yahoo.com.br>
2024-02-11 00:41:41 -0300

    Barra de navegação criada, mas ainda precisa de muitos ajustes

---
+++ php/estrutura.php
@@ -0,0 +1,18 @@
+<nav class="navbar">
+        <div class="logo">
+            <img src="../img/ResiReviveLogo.jpg" alt="Logo">
+        </div>
+        <div class="search">
+            <input type="text" placeholder="Buscar">
+            <button type="submit">Buscar</button>
+        </div>
+        <div class="menu">
+            <a href="#">Santarém | PA</a>
+            <a href="#">Meus Anúncios</a>
+            <a href="#">Conversas</a>
+            <a href="#">Notificações</a>
+        </div>
+        <div class="login">
+            <button>Entrar</button>
+        </div>
+    </nav>
