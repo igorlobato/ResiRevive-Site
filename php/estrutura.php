1baa38e206abacf6e0dc4fd9df25ab6e32e45b6f
Igor <igor_stm@yahoo.com.br>
2024-02-22 00:04:45 -0300

    Front-End do Menu Superior concluído

--- php/estrutura.php
+++ php/estrutura.php
@@ -1,18 +1,22 @@
 <nav class="navbar">
-        <div class="logo">
-            <a href="index.php"><img src="../img/ResiReviveLogo.jpg" alt="Logo"></a>
+    <div class="logo">
+        <a href="index.php"><img src="../img/ResiReviveLogo.jpg" alt="Logo"></a>
+    </div>
+    <div class="search">
+        <div class="input-group">
+            <input type="text" class="form-control" placeholder="Buscar" style="border-radius: 10px; border: none; background-color: #e9e9e9;">
+            <div class="input-group-append">
+                <a href="index.php" class="logo"><img src="../img/ícones/lupa.png"></a>
+            </div>
         </div>
-        <div class="search">
-            <input type="text" placeholder="Buscar">
-            <button type="submit">Buscar</button>
-        </div>
-        <div class="menu">
-            <a href="#">Santarém | PA</a>
-            <a href="#">Meus Anúncios</a>
-            <a href="#">Conversas</a>
-            <a href="#">Notificações</a>
-        </div>
-        <div class="login">
-            <a href="login.php" id="entrar"><button>Entrar</button></a>
-        </div>
-    </nav>
+    </div>
+    <div class="menu">
+        <a href="#" style="margin-left: 400px;">Santarém | PA</a>
+        <a href="#" style="margin-left: 50px;">Meus Anúncios</a>
+        <a href="#" style="margin-left: 50px;">Conversas</a>
+        <a href="#" style="margin-right: 95px; margin-left: 50px;">Notificações</a>
+    </div>
+    <div class="login">
+        <a href="login.php" id="entrar"><button>Entrar</button></a>
+    </div>
+</nav>
